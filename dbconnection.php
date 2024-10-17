7f79d3a67040f3407f475cc4799f7cd9829fef22
pascalongeri <pascalongeri@gmail.com>
2024-10-17 10:56:01 +0300

    Your descriptive commit message

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,8 @@
 //$con=mysqli_connect("db4free.net", "shilambilashaban", "shilambilashaban", "shilambilashaban");
 
 
-$con=mysqli_connect("localhost", "vpbgvvdz_simon", "40702314Simon?", "vpbgvvdz_pay");
+//$con=mysqli_connect("localhost", "vpbgvvdz_simon", "40702314Simon?", "vpbgvvdz_pay");
+$con=mysqli_connect("173.249.20.229", "remote", "Qwerty245!", "schoolsystem");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
